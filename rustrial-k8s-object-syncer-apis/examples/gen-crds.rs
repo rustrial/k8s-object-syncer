e4f31cb454f58be8d01953aef665d6adc008b414
Alexander Berger <alex.berger@nexxiot.com>
2026-03-30 10:45:11 +0200

    Update dependencies and bump Alpine base image
    - Upgrade kube 3.0.1 -> 3.1.0, k8s-openapi 0.27.0 -> 0.27.1,
      kube-runtime 3.0.1 -> 3.1.0
    - Replace deprecated serde_yaml with serde_yaml_ng 0.10
    - Bump env_logger, tokio, futures, rustls, anyhow to latest
    - Bump Alpine base image 3.22.2 -> 3.23
    - Update Cargo.lock with all transitive dependency updates

--- rustrial-k8s-object-syncer-apis/examples/gen-crds.rs
+++ rustrial-k8s-object-syncer-apis/examples/gen-crds.rs
@@ -1,7 +1,7 @@
 use kube::CustomResourceExt;
 use rustrial_k8s_object_syncer_apis::ObjectSync;
-use serde_yaml;
+use serde_yaml_ng;
 
 pub fn main() {
-    println!("{}", serde_yaml::to_string(&ObjectSync::crd()).unwrap());
+    println!("{}", serde_yaml_ng::to_string(&ObjectSync::crd()).unwrap());
 }
